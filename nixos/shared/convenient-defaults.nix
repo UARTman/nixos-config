f72fed9cca81487c75196536e095216f6dd45e87
Anton Gusev <uartman@mail.ru>
2025-08-12 14:19:15 +0300

    fix missing networkmanager openvpn support

--- nixos/shared/convenient-defaults.nix
+++ nixos/shared/convenient-defaults.nix
@@ -194,6 +194,9 @@
 
     # Enable networking
     networking.networkmanager.enable = true;
+    networking.networkmanager.plugins = with pkgs; [
+      networkmanager-openvpn
+    ];
 
     # Set your time zone.
     time.timeZone = "Europe/Moscow";
