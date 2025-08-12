5aed1c01e08e3f9659b58f7f4e606bcc42210972
Anton Gusev <uartman@mail.ru>
2025-08-12 14:37:50 +0300

    refactor syncthing devices into syncthing file

--- nixos/shared/convenient-defaults.nix
+++ nixos/shared/convenient-defaults.nix
@@ -183,19 +183,6 @@
           "1d71939404bd0816"
         ];
       };
-
-      syncthing.settings.devices = {
-        "uartpc" = {
-          id = "CY5YEK2-BCLLIQI-2S7RDTQ-5UTH7TW-OSH57RK-HZ46Q5D-ANJD4RI-B24PKAY";
-        };
-        "pixel" = {
-          id = "YOABTVV-RXHTLM2-SVLE6OG-DXOYLFE-ZQ7LVGR-3SCXZDF-D7EUTKZ-2PHQ3QI";
-        };
-        "owlbear" = {
-          id = "2HPWTOS-4CFTD67-5FBDEDY-36LHYLC-E7NN3SL-2EH5TRP-5M6V6JA-ZOO5BAW";
-        };
-      };
-
     };
 
     # Enable networking
--- nixos/shared/syncthing.nix
+++ nixos/shared/syncthing.nix
@@ -4,5 +4,16 @@
   services.syncthing = {
     enable = true;
     openDefaultPorts = true;
+    devices = {
+      "uartpc" = {
+        id = "CY5YEK2-BCLLIQI-2S7RDTQ-5UTH7TW-OSH57RK-HZ46Q5D-ANJD4RI-B24PKAY";
+      };
+      "pixel" = {
+        id = "YOABTVV-RXHTLM2-SVLE6OG-DXOYLFE-ZQ7LVGR-3SCXZDF-D7EUTKZ-2PHQ3QI";
+      };
+      "owlbear" = {
+        id = "2HPWTOS-4CFTD67-5FBDEDY-36LHYLC-E7NN3SL-2EH5TRP-5M6V6JA-ZOO5BAW";
+      };
+    };
   };
 }
