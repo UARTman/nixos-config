9b261d8c3bec7fdd9c0ef6e2b1a7cc331051cdbe
Anton Gusev <uartman@mail.ru>
2025-09-23 00:31:22 +0300

    add basic config for owlbear

--- nixos/mimic/configuration.nix
+++ nixos/mimic/configuration.nix
@@ -9,8 +9,11 @@
 
 {
   imports = [
+    ./hardware-configuration.nix
     ../shared/convenient-defaults.nix
   ];
+
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.hostName = "mimic";
 
@@ -19,8 +22,87 @@
 
   users.users.uartman = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ];
-    initialPassword = "test";
+    extraGroups = [
+      "wheel"
+      "networkmanager"
+      "docker"
+      "kvm"
+      "adbusers"
+      "harddrives"
+    ];
+  };
+
+  users.groups.harddrives = {
+    name = "harddrives";
+  };
+
+  time.hardwareClockInLocalTime = true;
+
+  environment.systemPackages = with pkgs; [
+    self.inputs.nix-alien.packages.x86_64-linux.nix-alien
+    mangohud
+  ];
+
+  programs.nix-ld = {
+    enable = true;
+    libraries = with pkgs; [
+      # fontconfig
+      # wayland
+      # libX11
+      (pkgs.runCommand "steamrun-lib" { } "mkdir $out; ln -s ${steam-run.fhsenv}/usr/lib64 $out/lib")
+      mangohud
+    ];
+  };
+
+  fonts.packages = with pkgs; [
+    arkpandora_ttf
+  ];
+
+  services.syncthing = {
+    user = "uartman";
+    dataDir = "/home/uartman/";
+    configDir = "/home/uartman/.config/syncthing";
+    settings = {
+      folders = {
+        "Main Obsidian Vault" = {
+          path = "/home/uartman/Documents/Obsidian vaults/Main Vault/";
+          devices = [
+            "uartpc"
+            "pixel"
+          ];
+        };
+        "Shared Music" = {
+          path = "/home/uartman/Music/Shared/";
+          devices = [
+            "uartpc"
+            "pixel"
+          ];
+        };
+      };
+    };
+  };
+
+  systemd.oomd = {
+    enableRootSlice = true;
+    enableUserSlices = true;
+  };
+
+  fileSystems."/mnt/windows" = {
+    device = "/dev/disk/by-uuid/E47221BA7221927A";
+    fsType = "ntfs3";
+    options = [ "discard" "rw" "uid=root" "gid=harddrives" "users" "umask=002" ];
+  };
+
+  fileSystems."/mnt/harddrive" = {
+    device = "/dev/disk/by-uuid/00EC74173638A268";
+    fsType = "ntfs3";
+    options = [ "discard" "rw" "uid=root" "gid=harddrives" "users" "umask=002" ];
+  };
+
+  fileSystems."/mnt/ssd" = {
+    device = "/dev/disk/by-uuid/461C9DF81C9DE36B";
+    fsType = "ntfs3";
+    options = [ "discard" "rw" "uid=root" "gid=harddrives" "users" "umask=002" ];
   };
 
   system.stateVersion = "24.05";
